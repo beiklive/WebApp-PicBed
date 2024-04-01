e388887c56f36276c41fc0e0b394c63bb8c535e9
beiklive <1376896787@qq.com>
2024-04-01 21:38:32 +0800

    添加相册路由

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,9 @@ from utils import RouterHandle
 def make_app():
     return tornado.web.Application([
         (r"/api", RouterHandle.APIHandler),
+        (r"/albums/(\d+)/photos/(\d+)", RouterHandle.PhotosHandler),    # http://localhost:8961/albums/123/photos/456
+        (r"/albums/(\d+)/photos", RouterHandle.PhotosHandler),          # http://localhost:8961/albums/123/photos
+        (r"/albums/(\d+)?", RouterHandle.AlbumsHandler),                # http://localhost:8961/albums/123
         (r"/404", RouterHandle.NotFound404Handler),
         # 静态文件路由必须放在最后，否则会拦截所有请求
         (r"/(.*)", tornado.web.StaticFileHandler, {"path": "web/dist", "default_filename": "index.html"}),
--- backend/utils/RouterHandle.py
+++ backend/utils/RouterHandle.py
@@ -19,4 +19,21 @@ class NotFound404Handler(tornado.web.RequestHandler):
     def get(self):
         print("NotFound404Handler")
         self.write("Hello, NotFound404Handler")
-        
+
+
+
+class PhotosHandler(tornado.web.RequestHandler):
+    def get(self, album_id=None, photo_id=None):
+        if album_id and photo_id:
+            self.write(f"Showing photo with ID {photo_id} from album with ID {album_id}")
+        elif album_id:
+            self.write(f"Showing all photos from album with ID {album_id}")
+        else:
+            self.write("Invalid URL")
+
+class AlbumsHandler(tornado.web.RequestHandler):
+    def get(self, album_id=None):
+        if album_id:
+            self.write(f"Showing album with ID {album_id}")
+        else:
+            self.write("Showing all albums")
